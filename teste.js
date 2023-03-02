1fb21c6c4558c6a2baaf52b3f0c995f4994d5d3b
NicoleLima0 <nick01tererj@gmail.com>
2023-03-02 11:19:49 -0300

    Initial commit

---
+++ teste.js
@@ -0,0 +1,24 @@
+function fibonacci(n) {
+  let fib = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
+}
+
+function sequenciafibonacci(n) {
+  let fib = fibonacci(n);
+  let index = fib.indexOf(n);
+
+  if (index === -1) {
+    return `${n} O número informado não pertence a sequência Fibonacci.`;
+  } else {
+    return `${n} O número informado pertence a sequência Fibonacci ${index}.`;
+  }
+}
+
+let number = parseInt(prompt("Coloque um número: "));
+let message = sequenciafibonacci(number);
+console.log(message);
